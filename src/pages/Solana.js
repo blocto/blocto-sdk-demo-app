b31fae710da257400c3f899a9abdb0f4f0a95749
Yang Lin <yanglin5689446@gmail.com>
2021-08-23 17:46:30 +0800

    Add solana create account test

--- src/pages/Solana.js
+++ src/pages/Solana.js
@@ -20,8 +20,10 @@ const Solana = () => {
   const [sendTo, setSendTo] = useState('');
   const [amount, setAmount] = useState('');
   const [newAccount, setNewAccount] = useState(null);
+  const [newAccount2, setNewAccount2] = useState(null);
   const [transactionStatus, setTransactionStatus] = useState('IDLE');
   const [partialSignTxStatus, setPartialSignTxStatus] = useState('IDLE');
+  const [partialSignTx2Status, setPartialSignTx2Status] = useState('IDLE');
   const [txHash, setTxHash] = useState(null);
 
   useEffect(() => {
@@ -130,6 +132,52 @@ const Solana = () => {
     } catch (e) {
       console.error(e);
       setPartialSignTxStatus('FAILED')
+    }
+  }, [account]);
+
+  const sendPartialSignTransaction2 = useCallback(async (e) => {
+    if(!sdk.current)return;
+    const { solana } = sdk.current
+    e.preventDefault();
+
+    const { value: { blockhash } } = await solana.request({ method: 'getRecentBlockhash' });
+    const transaction = new Transaction();
+    const publicKey = new PublicKey(account);
+
+    const newKeypair = new Keypair();
+    const newAccountKey = newKeypair.publicKey;
+
+    const rent = await solana.request({ method: 'getMinimumBalanceForRentExemption', params: [10] })
+
+    const createAccountInstruction = SystemProgram.createAccount({
+      fromPubkey: publicKey,
+      newAccountPubkey: newAccountKey,
+      lamports: rent,
+      // create an account with newly-generated key, and assign it to system program
+      programId: SystemProgram.programId,
+      space: 10,
+    });
+    transaction.add(createAccountInstruction);
+
+    setNewAccount2(newAccountKey.toBase58());
+
+    transaction.feePayer = publicKey;
+    transaction.recentBlockhash = blockhash;
+
+    try {
+      const converted = await solana.convertToProgramWalletTransaction(transaction)
+      converted.partialSign(newKeypair);
+
+      setPartialSignTx2Status('PENDING');
+
+      const response = await solana.signAndSendTransaction(converted)
+      setTxHash(response);
+      setPartialSignTx2Status('SUCCESS');
+      solana.request({ method: 'getBalance', params: [account]})
+        .then(({ value }) => setBalance(value));
+    } catch (e) {
+      console.error(e);
+      setPartialSignTx2Status('FAILED')
     }
   }, [account]);
 
@@ -258,6 +306,54 @@ const Solana = () => {
               </button>
             </form>
           </div>
+
+          <hr />
+
+          <div className="card-body">
+            <h5 className="card-title">Create account</h5>
+            
+            <form onSubmit={sendPartialSignTransaction2}>
+              { !['IDlE', 'FAILED'].includes(partialSignTx2Status) && newAccount2 && (
+                <div className="alert alert-info">
+                  Created Account, PubKey: {newAccount2}
+                </div>
+              )}
+              { partialSignTx2Status === 'SUCCESS' && (
+                <div className="alert alert-success">
+                  Transaction Succeed!<br />
+                  Check your transaction {' '} 
+                  <a 
+                    href={`https://explorer.solana.com/tx/${txHash}?cluster=testnet`} 
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    here
+                  </a>
+                </div>
+              )}
+              { partialSignTx2Status === 'FAILED' && (
+                <div className="alert alert-danger">
+                  Something went wrong :'(
+                </div>
+              )}
+              <button
+                type="submit"
+                className="btn btn-primary"
+                disabled={partialSignTx2Status === 'PENDING'}
+              >
+                {partialSignTx2Status === 'PENDING' 
+                  ? (
+                    <>
+                      <span className="spinner-border spinner-border-sm me-1"/>
+                      <span>Sending</span>
+                    </>
+                  ) 
+                  : 'Send'
+                }
+              </button>
+            </form>
+          </div>
+
         </div>
       )}
     </div>
